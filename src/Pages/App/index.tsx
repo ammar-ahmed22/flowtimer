92b051522378bf1a73d3de0d7257df6e6574c17d
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-04-25 10:11:32 -0400

    chore: formatting

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,65 +1,67 @@
 import React, { useState, useEffect } from 'react'
-import { Container, HStack, Text, IconButton, } from '@chakra-ui/react'
+import { Container, HStack, Text, IconButton } from '@chakra-ui/react'
 import { ContextProvider } from '../../context'
 import ModeDisplay from '../../components/ModeDisplay'
 import { ColorModeSwitcher } from '../../ColorModeSwitcher'
 import Settings from '../../components/Settings'
-import { FaCircleInfo, FaArrowUpRightFromSquare, FaExpand } from 'react-icons/fa6'
+import {
+  FaCircleInfo,
+  FaArrowUpRightFromSquare,
+  FaExpand,
+} from 'react-icons/fa6'
 import { useLocation } from 'react-router-dom'
 
 const App: React.FC = () => {
   const location = useLocation()
-  const [isPopup, setIsPopup] = useState(false);
+  const [isPopup, setIsPopup] = useState(false)
 
   useEffect(() => {
     if (window.opener) {
-      setIsPopup(true);
+      setIsPopup(true)
     } else {
-      setIsPopup(false);
+      setIsPopup(false)
     }
   }, [])
 
   const requestFullScreen = (elem: Element) => {
-    let reqMethod = elem.requestFullscreen 
+    let reqMethod = elem.requestFullscreen
     if (reqMethod) {
-      reqMethod.call(elem);
+      reqMethod.call(elem)
     }
   }
 
   return (
     <ContextProvider>
-      <Container maxW='container.sm' centerContent minH='100vh' pos="relative" >
-        {
-          !isPopup && (
-            <IconButton 
-              icon={<FaArrowUpRightFromSquare />}
-              aria-label='New Window'
-              colorScheme="brandPurple"
-              size="sm"
-              fontSize="md"
-              variant="ghost"
-              pos="absolute"
-              bottom={4}
-              right={0}
-              onClick={() => {
-                window.open("/app", "_blank", "popup=yes,width=700,height=900")
-              }}
-            />
-          )
-        }
-        <IconButton 
+      <Container maxW='container.sm' centerContent minH='100vh' pos='relative'>
+        {!isPopup && (
+          <IconButton
+            icon={<FaArrowUpRightFromSquare />}
+            aria-label='New Window'
+            colorScheme='brandPurple'
+            size='sm'
+            fontSize='md'
+            variant='ghost'
+            pos='absolute'
+            bottom={4}
+            right={0}
+            onClick={() => {
+              window.open('/app', '_blank', 'popup=yes,width=700,height=900')
+            }}
+          />
+        )}
+        <IconButton
           icon={<FaExpand />}
           aria-label='Fullscreen'
-          colorScheme="brandPurple"
-          size="sm"
-          fontSize="md"
-          variant="ghost"
-          pos="absolute"
+          colorScheme='brandPurple'
+          size='sm'
+          fontSize='md'
+          variant='ghost'
+          pos='absolute'
           bottom={4}
           left={0}
           onClick={() => {
-            const root = document.getElementById("root");
-            if (root) requestFullScreen(document.body);
+            const root = document.getElementById('root')
+            if (root) requestFullScreen(document.body)
           }}
         />
         <HStack justify='space-between' w='100%' mt='3'>
